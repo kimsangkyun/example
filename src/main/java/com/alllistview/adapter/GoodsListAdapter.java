d64f0ca5feb2c56e318e00dc8cabd5504bd82db7
kimsangkyun <ksk1004zz@nate.com>
2015-03-12 10:23:59 +0900

    2015-03-11 2

--- src/main/java/com/alllistview/adapter/GoodsListAdapter.java
+++ src/main/java/com/alllistview/adapter/GoodsListAdapter.java
@@ -70,8 +70,6 @@ import java.util.ArrayList;
     return convertView;
   }
 
-  public void haha(){}
-
   @Override
   public int getChildrenCount(int groupPosition) {//������ŭ getChildView ȣ��
     // TODO Auto-generated method stub
